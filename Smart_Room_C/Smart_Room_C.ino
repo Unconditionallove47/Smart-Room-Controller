771c65f7229d4b47ae47323141ebb452a77a9918
kalif purce <kalifpurce@gmail.com>
2021-06-30 11:58:40 -0600

    Mostly Func

--- Smart_Room_C/Smart_Room_C.ino
+++ Smart_Room_C/Smart_Room_C.ino
@@ -16,91 +16,161 @@
 #include <Adafruit_GFX.h>
 #include <Wire.h>
 #include <math.h>
-const int PIXELCOUNT = 12;
-const int PIXELPIN = 17;
-bool status;
-float tempF = 0;
-float tempC;
-float pressinHg;
-float pressPA;
-float humidRH;
-int position;
-float tempGuage = tempF;
 #include <wemoObj.h>
-wemoObj myWemo;
-Adafruit_BME280 bme;
 #define LOGO_HEIGHT   1
 #define LOGO_WIDTH    1
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define SCREEN_WIDTH 128      // OLED display width, in pixels
+#define SCREEN_HEIGHT 64      // OLED display height, in pixels
+#define OLED_RESET     4      // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C     ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+const int PIXELCOUNT = 12;   //NEO PIXEL COUNT
+const int PIXELPIN = 17;     //NEO PIXEL PINS SETUP
+float tempF = 0;       // FOR BME
+float tempC;           //FOR BME C TO F
+float pressinHg;       //PRESSURE FOR BME
+float pressPA;         //PRESSURE FOR BME CONVERSION
+float humidRH;         //BME HUMIDITY
+float tempGuage = tempF;   //FOR TEMP CONV
+bool buttonState;         //FOR BUTTON FUNC
+bool status;
+bool status1;
+int position;           //BME AND NEO
+int rot = 2;
+int p;                 //INTEGER FOR PRESS X2
+int led = 13;       // define the LED pin  **FOR IR**
+int digitalPin = 21;      // KY-026 digital interface  **FOR IR**
+int analogPin = 20;      // KY-026 analog interface  **FOR IR**
+int digitalVal;      // digital readings  **FOR IR**
+int analogVal;      //analog readings  **FOR IR**
+wemoObj myWemo;
+Adafruit_BME280 bme;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-OneButton button1(23, false);
+EthernetClient client;
 Adafruit_NeoPixel pixel(PIXELCOUNT , PIXELPIN , NEO_GRB + NEO_KHZ800);
-int led = 13; // define the LED pin  **FOR IR**
-int digitalPin = 21; // KY-026 digital interface  **FOR IR**
-int analogPin = 20; // KY-026 analog interface  **FOR IR**
-int digitalVal; // digital readings  **FOR IR**
-int analogVal; //analog readings  **FOR IR**
+OneButton button(23, false);    //BUTTON SET TO FALSE
 
 void setup() {
+  pinMode(led, OUTPUT);   //FOR IR
+  pinMode(digitalPin, INPUT);    //FOR IR
   Serial.begin(9600);
-  pixel.begin();
-  pixel.show();
-  pixel.setBrightness(15);
-  (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) ;
-  status = bme.begin(0X76);
-  display.clearDisplay();
-  display.display();
-  delay(2000);
-  display.invertDisplay(true);
-  delay(1000);
-  display.invertDisplay(false);
-  delay(1000);
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+  status1 = Ethernet.begin(mac);           //ETHER BEGIN TO MAC
+  if (!status1) {
+    Serial.printf("failed to configure Ethernet using DHCP \n");
+    //no point in continueing
+    while (1);
+  }
+  Serial.print("My IP address:");
+  for (byte thisbyte = 0; thisbyte < 4; thisbyte++) {
+    //print value of each byte of the IP address
+    Serial.print(Ethernet.localIP()[thisbyte], DEC);
+    if (thisbyte < 3) Serial.print(".");
+  }
+  pixel.begin();                 // NEOPIXEL SETTING
+  pixel.show();                  //NEOPIXEL SETTING
+  pixel.setBrightness(15);       //NEOPIXEL SETTING
+  (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) ;      //OLED
+  status = bme.begin(0X76);          //BME START
+  display.clearDisplay();            //CLEARS DISP
+  display.display();                 //DISPLAYS DISP
+  display.invertDisplay(false);      //DISPLAY OPENING
+  display.setRotation(rot);          //DISPLAY ROTATION
+  button.attachClick(click);    //BUTTON CLICK
+  button.attachDoubleClick(doubleClick);
+  buttonState = false;           //BUTTON STATE
+  button.setPressTicks(250);    //BUTTON TICKS
 }
 
 void loop() {
-  position = tempF;
+  button.tick();
+  position = tempF;    //SETS POSITION TO THE TEMP
   tempGuage = map(position, 10, 120, 0, 12);  //encoder Pos to Position based on 0-95 being convered to 0-15(of available lights)
   pixel.clear();
   if (tempF < 68) {
-    pixel.fill( 0x0000FF, 0, tempGuage);
+    pixel.fill( 0x0000FF, 0, tempGuage);    //COLD TEMP
   }
-  else if (tempF >= 69 && tempF <= 80) {
+  else if (tempF >= 69 && tempF <= 80) {    //MED TEMP
     pixel.fill( 0x808000 , 0, tempGuage);
   }
   else {
-    pixel.fill(0xFF0000, 0, tempGuage);
+    pixel.fill(0xFF0000, 0, tempGuage);     //HOT TEMP
   }
   pixel.show();
-  tempC = bme.readTemperature(); //deg C
+  tempC = bme.readTemperature(); //deg C   //TEMP CONVERSION
   tempF = (tempC * 1.8) + 32;
   pressPA = bme.readPressure(); //pascals
-  pressinHg = (pressPA / 33.863886666667);
+  pressinHg = (pressPA / 33.863886666667);  //PRESSURE CONVERSION
   humidRH = bme.readHumidity(); //%RH
-  Serial.print("Temp=");
+  Serial.print("Temp=");            //PRINTS FOR TEMP ON SM
   Serial.print(tempF);
   Serial.println("°F");
   delay(1000);
-  Serial.print("pressure=");
+  Serial.print("pressure=");        //PRINTS FOR PRESSURE ON SM
   Serial.print(pressinHg / 100.0F);
   Serial.println("hPa");
   delay(1000);
-  Serial.print("Humidity=");
+  Serial.print("Humidity=");         //PRINTS FOR HUMIDITY ON SM
   Serial.print(bme.readHumidity());
   Serial.println("%");
   delay(1000);
-  testdrawstyles();
+  testdrawstyles();        //OLED DISPLAY TEXT STYLE
+//  if (tempF < 68 ) {
+//    setHue(3, true, HueBlue, 103, 247);
+//  }
+//  else if (tempF >= 69 && tempF <= 80 ) {
+//    setHue(3, true, HueYellow, 103, 247);
+//  }
+//  else
+//    setHue(3, true, HueRed, 103, 247);
+
+  // Read the digital interface
+  digitalVal = digitalRead(digitalPin);
+  if (digitalVal == HIGH) // if flame is detected
+  {
+    digitalWrite(led, HIGH); // turn ON Arduino's LED
+  }
+  else
+  {
+    digitalWrite(led, LOW); // turn OFF Arduino's LED
+  }
+  // Read the analog interface
+  analogVal = analogRead(analogPin);
+  Serial.println(analogVal); // print analog value to serial
+  delay(100);
+  //  if (tempF > 80) {
+  //    myWemo.switchON (0);     //temp over 80 "fan" on
+  //  }
+  //  else {
+  //      myWemo.switchOFF (0);
+  //    }
+  //  if (tempF >= 69 && tempF <= 79) {       //If temp is between 69 and 80 "heater" on
+  //    myWemo.switchON (1);
+  //  }
+  //  else {
+  //      myWemo.switchOFF (1);              //else turn it off
+  //    }
 }
 
-void testdrawstyles(void) {
+void testdrawstyles(void) {            //VOID FOR OLED DISPLAY TEXT
   display.clearDisplay();
-  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK); // Draw 'inverse' text
+  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK);   // Draw 'inverse' text
   display.setTextSize(1);
   display.setCursor(0, 0);
   display.printf("temp=%f \n Pressure=%f \n Humidity=%f\n", tempF, pressinHg / 100.0F, bme.readHumidity());
   display.display();
-  delay(2000);
 
+}
+void click() {                   //VOID FOR BUTTON CLICK
+  buttonState = !buttonState;
+  Serial.println("Single Press");
+}
+void doubleClick() {        //void for BUTTON DOUBLE CLICK
+  p++;
+  Serial.println("Double Press");
+  if (p > 2) {
+    (p = 0);
+  }
 }
